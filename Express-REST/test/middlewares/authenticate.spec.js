5493c93bb8ab9d70aa3bcb68093b85be525bb411
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-01-31 15:25:14 +0100

    Correction

--- Express-REST/test/middlewares/authenticate.spec.js
+++ Express-REST/test/middlewares/authenticate.spec.js
@@ -3,17 +3,18 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const expect = chai.expect;
+const user = require('../../models/user');
 
 chai.use(sinonChai);
 
 describe('middleware authenticate', () => {
   it('should create status 401 when user is not logged', () => {
     const req = {
-      headers: {}
+      headers: {},
     };
     const res = {
       json: sinon.spy(),
-    }
+    };
     authenticate(req, res);
     expect(res.statusCode).to.equal(401);
     expect(res.json).to.have.been.calledOnceWithExactly({
@@ -22,6 +23,15 @@ describe('middleware authenticate', () => {
   });
 
   it('should call next when user is logged', () => {
-    // Exercice
+    user.tokens.push('1234');
+    const req = {
+      headers: {
+        authorization: '1234'
+      },
+    };
+    const res = {};
+    const next = sinon.spy();
+    authenticate(req, res, next);
+    expect(next).to.have.been.calledWithExactly();
   });
 });
